8f23f4c243018ce8ab39bd7260f5e12e74b94e58
NagaVaishnavi-Puppala <puppala.n@northeastern.edu>
2023-10-22 19:00:26 -0400

    Modified flyway.conf and create table

--- flyway/sql/V1__Create_Table.sql
+++ flyway/sql/V1__Create_Table.sql
@@ -1,9 +1,14 @@
-CREATE TABLE app.users (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  username VARCHAR(50) NOT NULL UNIQUE,
-  password VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE http_checks (
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  uri VARCHAR(255) NOT NULL,
+  is_paused BOOLEAN NOT NULL,
+  num_retries INTEGER NOT NULL CHECK (num_retries >= 0 AND num_retries <= 5),
+  uptime_sla INTEGER NOT NULL CHECK (uptime_sla >= 0 AND uptime_sla <= 100),
+  response_time_sla INTEGER NOT NULL CHECK (response_time_sla >= 0 AND response_time_sla <= 100),
+  use_ssl BOOLEAN NOT NULL,
+  response_status_code INTEGER NOT NULL DEFAULT 200,
+  check_interval_in_seconds INTEGER NOT NULL CHECK (check_interval_in_seconds >= 0 AND check_interval_in_seconds <= 86400),
+  check_created TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+  check_updated TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
 );
